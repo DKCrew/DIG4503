c73176b0bec5ce6638ed07da005f57c75f9a4190
DKCrew <jwilliams95@knights.ucf.edu>
2021-01-29 17:45:51 -0500

    FINAL final

--- Lab02/index.js
+++ Lab02/index.js
@@ -1,18 +1,18 @@
-const axios = require("axios");
+const axios = require("axios"); //calls axios to run
 
-    axios('http://pokeapi.co/api/v2/pokemon/gengar')
+    axios('http://pokeapi.co/api/v2/pokemon/gengar') //accesses the created api for data on pokemon
 
-        .then(function (response)
+        .then(function (response) //function to receive the api response
 
         {
-            const pokemon = response.data;
+            const pokemon = response.data; //creates a constant to store the pokemon data
 
             console.log("This pokemon's name is " + pokemon.name + " and its pokedex number is " + pokemon.id);
-
+            //logs result from API
         }
     )
 
-    .catch(function(error){
+    .catch(function(error){ //function to display error if Api result is not found (pokemon not listed etc.)
         console.log("There has been an Error: " + error);
     }
     );
