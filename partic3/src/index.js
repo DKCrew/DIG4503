f421217204cd432033cb493eef5ec0b5a86f835e
DKCrew <jwilliams95@knights.ucf.edu>
2021-02-05 19:31:37 -0500

    Working commit

---
+++ partic3/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+const customer = {
+
+  first_name: "Jeremy",
+
+  last_name: "W"
+
+};
+
+
+const output = <div> <h1>My name is "console.log(customer.first_name)</h1>
+<h2>My last name is console.log(customer.last_name)</h2>
+</div>;
+
+ReactDOM.render(output, document.querySelector("#root"));
