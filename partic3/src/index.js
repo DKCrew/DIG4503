754c23cb22cd080a2885e77b9879b22bda9b7359
DKCrew <jwilliams95@knights.ucf.edu>
2021-02-05 19:37:26 -0500

    Working commit

--- partic3/src/index.js
+++ partic3/src/index.js
@@ -11,8 +11,9 @@ const customer = {
 };
 
 
-const output = <div> <h1>My name is "console.log(customer.first_name)</h1>
-<h2>My last name is console.log(customer.last_name)</h2>
-</div>;
+const output = 
+  <h1>My name is </h1>; console.log(customer.first_name);
+<h2>My last name is </h2>; console.log(customer.last_name);
+
 
 ReactDOM.render(output, document.querySelector("#root"));
