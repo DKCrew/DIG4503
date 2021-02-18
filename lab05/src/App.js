06114db0f35f6bc55032e5ed5b51ee3fd0375aa3
DKCrew <jwilliams95@knights.ucf.edu>
2021-02-18 15:35:02 -0500

    Final Commit

---
+++ lab05/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Posts from './components/post'
+
+
+function App() {
+  
+    return(
+      <Posts/>
+    )
+}
+
+export default App;
+
---
+++ lab05/src/components/post.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+
+const Posts = () => {
+const [post, postCount] = useState(0);
+
+useEffect(() => {
+    console.log("Use effect ran");
+}, []);
+
+useEffect(() => {
+    console.log("Use effect ran");
+}, [post]);
+
+useEffect(() => {
+
+    console.log("Use effect ran");
+
+    return() => {
+
+console.log("return is being run");
+
+    }
+
+}, []);
+
+return(
+    <div>
+        <h1>Post</h1>
+        <p>Number of Posts: {post}</p>
+        <button onClick = { () => postCount(post + 1)}>Add a post</button>
+    </div>
+);
+
+};
+
+export default Posts
