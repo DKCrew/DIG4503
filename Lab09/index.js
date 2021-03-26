1c782572eac1adad0e85b51b7657c340f18f2fdc
DKCrew <jwilliams95@knights.ucf.edu>
2021-03-26 16:48:10 -0400

    Semi-working commit

---
+++ Lab09/index.js
@@ -0,0 +1,47 @@
+import Express from "express";
+import fs from "fs";
+
+const App = Express();
+const port = 3010;
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("public"));
+
+App.get("/employees/:name", (req, res) => {
+    let result = {"error": "Not Available"};
+
+    database.forEach((value) => {
+    if(req.params.name == value.name) {result = value;};
+});
+res.json(result);
+})
+
+App.get("/ages/:number", (req, res) => {
+
+    let result = {"error": "Not Available"};
+    database.forEach((value) => {
+        if(req.params.age == value.age) {result = value;};
+    });
+    res.json(result);
+})
+
+App.post("/employees/:name/:age", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result);
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+
+})
+
+App.listen(port, () => {
+    console.log("Server is running");
+})
